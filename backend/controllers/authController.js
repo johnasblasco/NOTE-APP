4c98099e83af8788cf491882783e7449bda18ec7
johnasblasco <johnaslblasco@gmail.com>
2025-06-06 15:03:37 +0800

    fix: explanation naman kupal

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,8 +8,11 @@ const JWT_SECRET = process.env.JWT_SECRET || 'johnasmaysecretmayanosyasatarubs';
 
 export const register = async (req, res) => {
   try {
+
+    // 1. get data from req.body
     const { name, email, password } = req.body;
 
+    // 2. backend validation
     if (!name || !email || !password) {
       return res.status(400).json({ message: 'All fields are required' });
     }
@@ -19,7 +22,9 @@ export const register = async (req, res) => {
       return res.status(400).json({ message: 'User already exists' });
     }
 
+    // 3. setup brcypt hash password
     const hashPassword = await bcrypt.hash(password, 10);
+    // 4. create user with hash password
     await User.create({ name, email, password: hashPassword });
 
     res.status(201).json({ message: 'User registered successfully' });
@@ -30,8 +35,11 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   try {
+
+    // 1. get data from req.body
     const { email, password } = req.body;
 
+    // 2. backend validation
     if (!email || !password) {
       return res.status(400).json({ message: 'Email and password are required' });
     }
@@ -46,8 +54,10 @@ export const login = async (req, res) => {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
 
+    // 3. generate token for login 
     const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '7d' });
 
+    // 4. send token to client
     res.status(200).json({
       token,
       user: { id: user._id, name: user.name, email: user.email }
