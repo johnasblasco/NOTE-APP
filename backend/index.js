4349ec98969a60fa87e13fb68fb93396e3bcef24
Chrismar Ilustrisimo <chrismarilustrisimo9@gmail.com>
2025-06-06 13:45:21 +0800

    fix: Deleted unwanted files:

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes.js';
+import mongoose from 'mongoose';
 import noteRoutes from './routes/noteRoutes.js';
+import authRoutes from './routes/authRoutes.js'
 import cors from 'cors';
 
 dotenv.config();
@@ -10,10 +11,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
+mongoose.connect(process.env.MONGO_URI, {})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => {
+  console.error('MongoDB connection error:', err);
+  process.exit(1);
+});
+
+
+
 app.use('/api/auth', authRoutes);
 app.use('/api/notes', noteRoutes);
 
-// Start server
-const PORT = process.env.PORT || 5000; // <- use PORT 5000(public) if our PORT in env (private) is not available
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ backend/model/User.js
@@ -0,0 +1,11 @@
+// model/User.js
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 3 },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+export default User; 
