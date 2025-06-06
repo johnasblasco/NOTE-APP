4ee013034c13020c5fda87ce170ae48a83e782c1
johnasblasco <johnaslblasco@gmail.com>
2025-06-06 15:03:49 +0800

    fix: wag na mag add ng mga kung ano ano perd

--- backend/model/User.js
+++ backend/model/User.js
@@ -5,7 +5,9 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 3 },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 6 }
-}, { timestamps: true });
+});
+
+//wag na timestop para linis tignan api natin pangit eh next time nalng
 
 const User = mongoose.model('User', userSchema);
 export default User; 
