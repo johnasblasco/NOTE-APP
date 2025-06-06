be4025da4f7e9042dfd8dee347a3ad75438c9be6
johnasblasco <johnaslblasco@gmail.com>
2025-06-06 14:51:15 +0800

    fix: cleaning and explaining the index setup

--- backend/index.js
+++ backend/index.js
@@ -8,20 +8,44 @@ import cors from 'cors';
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {})
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => {
-  console.error('MongoDB connection error:', err);
-  process.exit(1);
+//app.use is a middlewares it has two params (path, middleware) if path is blank then it will apply to all routes
+app.use(cors()); // applies to ALL routes
+app.use(express.json()); // applies to ALL routes
+app.use('/api/auth', authRoutes); // applies to /api/auth route
+app.use('/api/notes', noteRoutes); // applies to /api/notes route
+
+
+// get route for Health check route
+app.get('/', (req, res) => {
+    res.send('API is running... go to /api route');
 });
 
 
 
-app.use('/api/auth', authRoutes);
-app.use('/api/notes', noteRoutes);
+//setup database and server connection
+const startServer = async () => {
+    try {
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+        // Connect to MongoDB
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to MongoDB');
+
+
+        // Start the server
+        const PORT = process.env.PORT || 5000;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1);
+    }
+}
+
+
+
+// lastly start the server
+startServer();
+
