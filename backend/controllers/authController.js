9b8cb836c0337a8dcefb3df507ec40ea9642fea6
johnasblasco <johnaslblasco@gmail.com>
2025-06-08 18:14:13 +0800

    fix: botlog talga

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,6 +3,8 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../model/User.js';
 
+import dotenv from 'dotenv';
+dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET || 'johnasmaysecretmayanosyasatarubs';
 
